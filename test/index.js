c87672f834c1efeee2785c179e2bd82c9066dc0c
raaaahman <sylvain@devindetails.com>
2022-02-12 09:32:28 +0100

    test: Fonction 'getTodos'

--- test/index.js
+++ test/index.js
@@ -10,4 +10,29 @@ describe('La fonction "getTodos"', () => {
         expect(value).toBeDefined()
         expect(value).toBeInstanceOf(Array)
     })
+
+    it('Le tableau retourné contient un objet', () => {
+        const value = getTodos()
+
+        expect(value).toHaveSize(1)
+        expect(value).toBeInstanceOf(Object)
+    })
+
+    it('Le premier objet du tableau a une propriété "label" qui est une chaîne de caractère', () => {
+        const todo = getTodos()[0]
+
+        expect(todo).toEqual(jasmine.objectContaining({ label: jasmine.any(String) }))
+    })
+
+    it('Le premier objet du tableau a une propriété "complete" qui est un booléen', () => {
+        const todo = getTodos()[0]
+
+        expect(todo).toEqual(jasmine.objectContaining({ complete: jasmine.any(Boolean)}))
+    })
+
+    it('Le premier objet du tableau a une propriété "creationDate" qui est un objet natif de type "Date"', () => {
+        const todo = getTodos()[0]
+
+        expect(todo).toEqual(jasmine.objectContaining({ creationDate: jasmine.any(Date) }))
+    })
 })
