5354c883a1af9554c51ece90d9342c526b716ee5
raaaahman <sylvain@devindetails.com>
2022-02-13 12:05:10 +0100

    test: Tests des fonctions ajout, retrait et tri des todos

--- test/index.js
+++ test/index.js
@@ -1,38 +1,319 @@
-describe('La fonction "getTodos"', () => {
-    it('Est déclarée', () => {
-        expect(getTodos).toBeDefined()
-        expect(getTodos).toBeInstanceOf(Function)
+function containsObjectContaining(sample) {
+    return {
+        asymmetricMatch: function (actual) {
+            if(!(actual instanceof Array)) return false
+            
+            return !!actual.find(obj => jasmine.objectContaining(sample).asymmetricMatch(obj))
+        }
+    }
+}
+
+describe('Le tableau "myTodos"', () => {
+    it('Est déclaré', () => {
+        expect(myTodos).toBeDefined()
+        expect(myTodos).toBeInstanceOf(Array)
     })
 
-    it('Retourne un tableau', () => {
-        const value = getTodos()
-
-        expect(value).toBeDefined()
-        expect(value).toBeInstanceOf(Array)
-    })
-
-    it('Le tableau retourné contient un objet', () => {
-        const value = getTodos()
+    it('contient un objet', () => {
+        const value = myTodos
 
         expect(value).toHaveSize(1)
         expect(value).toBeInstanceOf(Object)
     })
 
-    it('Le premier objet du tableau a une propriété "label" qui est une chaîne de caractère', () => {
-        const todo = getTodos()[0]
+    it('cet objet a une propriété "id" qui est un nombre', () => {
+        const todo = myTodos[0]
+
+        expect(todo).toEqual(jasmine.objectContaining({ id: 1 }))
+    })
+
+    it('cet objet a une propriété "label" qui est une chaîne de caractère', () => {
+        const todo = myTodos[0]
 
         expect(todo).toEqual(jasmine.objectContaining({ label: jasmine.any(String) }))
     })
 
-    it('Le premier objet du tableau a une propriété "complete" qui est un booléen', () => {
-        const todo = getTodos()[0]
+    it('cet objet a une propriété "complete" qui est un booléen', () => {
+        const todo = myTodos[0]
 
         expect(todo).toEqual(jasmine.objectContaining({ complete: jasmine.any(Boolean)}))
     })
 
-    it('Le premier objet du tableau a une propriété "creationDate" qui est un objet natif de type "Date"', () => {
-        const todo = getTodos()[0]
+    it('cet objet a une propriété "creationDate" qui est un objet natif de type "Date"', () => {
+        const todo = myTodos[0]
 
         expect(todo).toEqual(jasmine.objectContaining({ creationDate: jasmine.any(Date) }))
+    })
+})
+
+describe('La fonction "setTodos"', () => {
+    it('Est déclarée', () => {
+        expect(setTodos).toBeDefined()
+        expect(setTodos).toBeInstanceOf(Function)
+    })
+
+    it('Accepte un paramètre qui est un tableau', () => {
+        expect(setTodos.length).toEqual(1)
+    })
+
+    it('Remplace le tableau stocké dans "myTodos" par le tableau passé en paramètre', () => {
+        const newTodos = [{
+            id: 2,
+            label: 'Hello World!',
+            complete: false,
+            creationDate: new Date()
+        }]
+
+        setTodos(newTodos)
+
+        expect(myTodos).toEqual(jasmine.arrayWithExactContents(newTodos))
+    })
+})
+
+describe('La fonction "createTodos"', () => {
+    it('Retourne un tableau', () => {
+        const value = createTodos()
+        
+        expect(value).toBeDefined()
+        expect(value).toBeInstanceOf(Array)
+    })
+
+    it('Le premier élément du tableau retourné correspond au tableau "myTodos"', () => {
+        const value = createTodos()
+
+        expect(value[0]).toEqual(jasmine.arrayWithExactContents(myTodos))
+    })
+
+    it('Le second élément du tableau retourné correspond à la fonction "setTodos"', () => {
+        const value = createTodos()
+
+        expect(value[1]).toEqual(setTodos)
+    })
+
+    it('Si l\'on passe un tableau en paramètre à la fonction "createTodos", ce tableau remplace les éléments contenus dans le tableau "myTodos"', () => {
+        const newTodos = [{
+            id: 3,
+            label: 'Hello World!',
+            complete: false,
+            creationDate: new Date()
+        }]
+
+        const value = createTodos(newTodos)
+
+        expect(value[0]).toEqual(jasmine.arrayWithExactContents(newTodos))
+    })
+})
+
+describe('La fonction "addTodo"', () => {
+    it('prend un objet en paramètre, et l\'ajoute au tableau "myTodos"', () => {
+        const newTodo = {
+            id: 4,
+            label: 'How are you today?',
+            complete: false,
+            creationDate: new Date()
+        }
+        addTodo(newTodo)
+
+        expect(myTodos).toContain(jasmine.objectContaining(newTodo))
+    })
+    
+    it('ne modifie pas directement le tableau "myTodos", mais appelle la fonction "setTodos" en lui passant un nouveau tableau en paramètre (voir concaténation de tableaux)', () => {
+        spyOn(window, 'setTodos')
+        
+        const todo1 = {
+            id: 4,
+            label: 'Hello World!',
+            complete: false,
+            creationDate: new Date()
+        }
+        myTodos = [todo1]
+        
+        const newTodo = {
+            id: 5,
+            label: 'Hello World!',
+            complete: false,
+            creationDate: new Date()
+        }
+        addTodo(newTodo)
+        
+        expect(setTodos).toHaveBeenCalledWith(jasmine.arrayContaining([
+            jasmine.objectContaining(todo1),
+            jasmine.objectContaining(newTodo)
+        ]))
+    })
+    
+})
+
+describe('La fonction "getTodoById"', () => {
+    it('retourne l\'élément du tableau "myTodos" dont la propriété "id" correspond au nombre passé en paramètre', () => {
+        const todo = {
+            id: 5,
+            label: 'Hello World!',
+            complete: false,
+            creationDate: new Date()
+        }
+        myTodos = [todo, {
+            id: 6,
+            label: 'How are you today?',
+            complete: false,
+            creationDate: new Date()
+        }]
+        const value = getTodoById(5)
+                
+        expect(value).toEqual(jasmine.objectContaining(todo))
+    })
+})
+
+describe('La fonction "removeTodo"', () => {
+    it('enlève l\'élément du tableau "myTodos" dont la propriété "id" correspond au nombre passé en paramètre ', () => {
+        const todo = {
+            id: 6,
+            label: 'Hello World!',
+            complete: false,
+            creationDate: new Date()
+        }
+        myTodos = [todo, {
+            id: 7,
+            label: 'How are you today?',
+            complete: false,
+            creationDate: new Date()
+        }]
+        removeTodo(6)
+
+        expect(myTodos).not.toContain(jasmine.objectContaining(todo))
+    })
+
+    it('ne modifie pas directement le tableau "myTodos", mais appelle la fonction "setTodos" en lui passant un nouveau tableau en paramètre', () => {
+        spyOn(window, 'setTodos')
+
+        const todo = {
+            id: 6,
+            label: 'Hello World!',
+            complete: false,
+            creationDate: new Date()
+        }
+        myTodos = [todo, {
+            id: 7,
+            label: 'How are you today?',
+            complete: false,
+            creationDate: new Date()
+        }]
+
+        removeTodo(7)
+
+        expect(setTodos).toHaveBeenCalledWith(jasmine.arrayWithExactContents([jasmine.objectContaining(todo)]))
+    })
+})
+
+describe('La fonction "filterTodosByComplete"', () => {
+    beforeAll(() => {
+        myTodos = [
+            {
+                id: 7,
+                label: 'Hello World!',
+                complete: false,
+                creationDate: new Date()
+            },
+            {
+                id: 8,
+                label: 'Hello World!',
+                complete: true,
+                creationDate: new Date()
+            },
+            {
+                id: 9,
+                label: 'Hello World!',
+                complete: true,
+                creationDate: new Date()
+            },
+            {
+                id: 10,
+                label: 'Hello World!',
+                complete: false,
+                creationDate: new Date()
+            },
+        ]
+    })
+
+    it('si l\'on ne passe pas de paramètre, retourne un tableau contenant uniquement les objets dont la propriété "complete" vaut "false"', () => {
+        const value = filterTodosByComplete()
+
+        expect(value).toContain(jasmine.objectContaining(myTodos[0]))
+        expect(value).toContain(jasmine.objectContaining(myTodos[3]))
+    })
+
+    it('si l\'on passe un paramètre qui est un booléen, retourne un tableau contenant uniquement les objets dont la propriété "complete" est égale au paramètre', () => {
+        const value1 = filterTodosByComplete(true)
+
+        expect(value1).toContain(jasmine.objectContaining(myTodos[1]))
+        expect(value1).toContain(jasmine.objectContaining(myTodos[2]))
+
+        const value2 = filterTodosByComplete(false)
+
+        expect(value2).toContain(jasmine.objectContaining(myTodos[0]))
+        expect(value2).toContain(jasmine.objectContaining(myTodos[3]))
+    })
+})
+
+describe('La fonction "sortTodosByCreationDate"', () => {
+    const todo1 = {
+        id: 11,
+        label: 'Hello World!',
+        complete: false,
+        creationDate: new Date(2021, 10, 12)
+    }
+    const todo2 = {
+        id: 12,
+        label: 'Hello World!',
+        complete: true,
+        creationDate: new Date(2022, 0, 16)
+    }
+    const todo3 = {
+        id: 13,
+        label: 'Hello World!',
+        complete: true,
+        creationDate: new Date(1999, 11, 01)
+    }
+    const todo4 = {
+        id: 10,
+        label: 'Hello World!',
+        complete: false,
+        creationDate: new Date(2002, 09, 20)
+    }
+
+    beforeAll(() => {
+        myTodos = [
+            todo1,
+            todo2,
+            todo3,
+            todo4,
+        ]
+    })
+
+    it('si l\'on ne passe pas de paramètre, retourne un tableau contenant les todos rangés par ordre de date décroissante (du plus récent au plus ancien)', () => {
+        const value = sortTodosByCreationDate()
+
+        expect(value[0]).toEqual(jasmine.objectContaining(todo2))
+        expect(value[1]).toEqual(jasmine.objectContaining(todo1))
+        expect(value[2]).toEqual(jasmine.objectContaining(todo4))
+        expect(value[3]).toEqual(jasmine.objectContaining(todo3))
+    })
+
+    it('si l\'on passe la chaîne "ASC" paramètre, retourne un tableau contenant les todos rangés par ordre de date croissante (du plus ancien au plus récent)', () => {
+        const value = sortTodosByCreationDate('ASC')
+
+        expect(value[0]).toEqual(jasmine.objectContaining(todo3))
+        expect(value[1]).toEqual(jasmine.objectContaining(todo4))
+        expect(value[2]).toEqual(jasmine.objectContaining(todo1))
+        expect(value[3]).toEqual(jasmine.objectContaining(todo2))
+    })
+
+    it('si l\'on passe la chaîne "DESC" en paramètre, retourne un tableau contenant les todos rangés par ordre de date décroissante (du plus récent au plus ancien)', () => {
+        const value = sortTodosByCreationDate('DESC')
+
+        expect(value[0]).toEqual(jasmine.objectContaining(todo2))
+        expect(value[1]).toEqual(jasmine.objectContaining(todo1))
+        expect(value[2]).toEqual(jasmine.objectContaining(todo4))
+        expect(value[3]).toEqual(jasmine.objectContaining(todo3))
     })
 })
